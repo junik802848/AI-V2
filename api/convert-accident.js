672a772a5bff13d595f9a97e32ebb2ab127ba832
이준익 <junik802848@gmail.com>
2026-09-10 11:13:13 +0900

    Fix accident presentation upload handler

--- api/convert-accident.js
+++ api/convert-accident.js
@@ -1,4 +1,5 @@
 const { put } = require('@vercel/blob');
+const { uploadAndRecord } = require('./_supabase');
 
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
